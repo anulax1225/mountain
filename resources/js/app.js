8bc71b7f9377e1e2b0cad0591a21e774e21aa578
anulax <anulax1225@icloud.com>
2025-10-22 08:07:04 +0200

    feat: created a js adapater file and modified theme

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,23 +1,32 @@
-import './bootstrap';
 import Alpine from 'alpinejs';
 import { setupVimeshUI } from '@vimesh/ui/dist/vui.mjs';
-import tash from 'alpinejs-tash'
+import tash from 'alpinejs-tash';
+import focus from '@alpinejs/focus';
 
 window.Alpine = Alpine;
 
-Alpine.plugin(tash)
-
 setupVimeshUI(window);
-
 $vui.config = {
     namespace: "ui",
     importMap: {
         "*": "/ui/components/${component}.html",
+        "icon": "/ui/icons/${component}.html",
         "page": "/ui/pages/${component}.html",
         "element": "/ui/elements/${component}.html"  
     },
     debug: true,
     autoImport: true,
 };
+$vui.Component = (component, el) => {
+    component.prop = (name, fallback) => {
+        let comp = $vui.findClosestComponent(el)
+        if (!comp) return null
+        return Alpine.bound(comp, `${name}`, fallback)
+    };
+    return component;
+};
+
+Alpine.plugin(tash);
+Alpine.plugin(focus);
 
 Alpine.start();
