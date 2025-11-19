78dd396bbb95e45d35daf43dbf5a47722c206727
anulax <anulax1225@icloud.com>
2025-11-19 08:54:33 +0100

    feat: working on unwrapping functionalities

--- resources/js/alpine-compositor/cregistery.js
+++ resources/js/alpine-compositor/cregistery.js
@@ -1,6 +1,6 @@
 import evaluateScriptSetup from "./evaluator.js";
 import Alpine from "../alpinejs";
-import { ref, reactive, computed, effect, propsBuilder  } from "./utils.js"
+import { ref, reactive, computed, effect, propsBuilder } from "./utils.js"
 
 const registeredComponents = new Set();
 const componentStyleSheet = new CSSStyleSheet();
@@ -35,25 +35,25 @@ export function registerStyleSheet(sheet) {
 }
 
 export function hasComponent(name) {
-    registeredComponents.has(name);
+    return registeredComponents.has(name);
 }
 
 // from https://github.com/vimeshjs/vimesh-ui - src/x-component.js
 function copyAttributes(elFrom, elTo) {
     const DIR_DATA = Alpine.prefixed('data');
     const DIR_INIT = Alpine.prefixed('init');
+    const DIR_COMP = Alpine.prefixed('component');
+    const reject = [DIR_COMP, DIR_DATA, "unwrap"];
     Array.from(elFrom.attributes).forEach(attr => {
-        //if (DIR_COMP === attr.name || attr.name.startsWith(DIR_COMP)) return
+        if (reject.includes(attr.name)) return;
         try {
-            let name = attr.name
+            let name = attr.name;
             if (name.startsWith('@'))
                 name = `${Alpine.prefixed('on')}:${name.substring(1)}`
             else if (name.startsWith(':'))
                 name = `${Alpine.prefixed('bind')}:${name.substring(1)}`
             if (DIR_INIT === name && elTo.getAttribute(DIR_INIT)) {
                 elTo.setAttribute(name, attr.value + ';' + elTo.getAttribute(DIR_INIT))
-            } else if (DIR_DATA === name) {
-                return;
             } else if ('class' === name) {
                 elTo.setAttribute(name, attr.value + ' ' + (elTo.getAttribute('class') || ''))
             } else if (!elTo.hasAttribute(name)) {
@@ -63,6 +63,33 @@ function copyAttributes(elFrom, elTo) {
             console.warn(`Fails to set attribute ${attr.name}=${attr.value} in ${elTo.tagName.toLowerCase()}`)
         }
     })
+}
+
+function processComponent(node) {
+    const component = node.tagName === "TEMPLATE" ? node.content.cloneNode(true) : node.cloneNode(true);
+    return { component, slots: Array.from(component.querySelectorAll("slot")) };
+}
+
+function scopeElements(elements, parent) {
+    Array.from(elements).filter(el => !el._x_dataStack).forEach(el => {
+        Alpine.addScopeToNode(el, {}, parent);
+    });
+}
+
+function applySlots(slots, parent) {
+    if (slots) {
+        const scopable = parent.querySelectorAll("*");
+        const defaultContent = Array.from(parent.children).filter(el => el.tagName !== 'template');
+        Array.from(parent.children).forEach(child => child.remove());
+        const textContent = parent.textContent;
+        parent.textContent = "";
+        scopeElements(scopable, parent);
+        slots.forEach(slot => {
+            if (!slot.hasAttribute("name")) {
+                slot.replaceWith(...defaultContent, textContent);
+            }
+        })
+    }
 }
 
 export function registerComponent(el, componentName, setupScript = "return {}") {
@@ -88,87 +115,76 @@ export function registerComponent(el, componentName, setupScript = "return {}") 
         return;
     }
 
+    const { component, slots } = processComponent(el);
     class AlpineWebComponent extends HTMLElement {
         constructor() {
             super();
-            this.shadowMode = true;
             console.log(`[Alpine Component] ${componentName} should unwrap ${el.hasAttribute("unwrap")}`)
-            this.unwrap = false;
-            this.shadow = this.shadowMode ? this.attachShadow({ mode: 'open' }) : null;
-            const { component, slots } = this.processComponent(el);
-            this._slots = slots;
-            this._component = component;
-            console.log(this._slots); 
-            if (this.shadowMode) {
-                this.shadow.adoptedStyleSheets = sheets;
-                this.root = this.shadow;
-            } else {
-                this.root = this;
-            }
+            this._m_unwrap = el.hasAttribute("unwrap");
+            this._m_shadowMode = !this._m_unwrap;
+            this._m_shadow = this._m_shadowMode ? this.attachShadow({ mode: 'open' }) : null;
+            this._m_slots = slots;
+            this._m_component = component;
+            if (this._m_shadowMode) {
+                this._m_shadow.adoptedStyleSheets = sheets;
+                this._m_root = this._m_shadow;
+            } else this._m_root = this;
+            this._m_test = "test";
         }
 
         connectedCallback() {
-            console.log(`[Alpine Component] Initializing new ${componentName}`);
-            copyAttributes(el, this);
-            this.setAttribute("x-root", "");
-            const props = Alpine.reactive({});
+            let root = this;
+            if (this._m_unwrap) {
+                const firstChild = el.tagName === "TEMPLATE" ? el.content.firstElementChild.cloneNode(true) : el.firstElementChild.cloneNode(true);
+                copyAttributes(this, firstChild);
+                this._m_root = firstChild;
+                const { component, slots } = processComponent(firstChild);
+                this._m_slots = slots;
+                this._m_component = component.children;
+                firstChild._m_unwrappedRef = this;
+                root = firstChild;
+                firstChild.innerHTML = "";
+                firstChild.append(...this.children);
+                const parent = this.parentNode.host ? this.parentNode.host : this.parentElement;
+                (parent._m_shadow ?  parent._m_shadow : parent).append(firstChild);
+                this.remove();
+                Alpine.onElRemoved(firstChild, () => {
+                    root._m_reactiveData['destroy'] && Alpine.evaluate(this, this._m_reactiveData['destroy'])
+                    root._m_undo()
+                });
+            }
 
-            let data = evaluateScriptSetup(this, setupScript, [], 
-                { 
-                    $host: this, 
-                    $shadow: this.shadow,
-                    defineProps: propsBuilder(this, props), 
-                    ref, reactive, effect, computed, 
+            console.log(`[Alpine Component] Initializing new ${componentName}`);
+            copyAttributes(el, root);
+            root.setAttribute("x-root", "");
+
+            root._m_props = Alpine.reactive({});
+            let data = evaluateScriptSetup(root, setupScript, [],
+                {
+                    $host: root,
+                    $shadow: this._m_shadow,
+                    defineProps: propsBuilder(root, root._m_props),
+                    ref, reactive, effect, computed,
                 },
             ) || {};
-            data = { $props: props, ...data };
-            Alpine.injectMagics(data, this);
-            this.reactiveData = Alpine.reactive(data);
-            Alpine.initInterceptors(this.reactiveData);
-            this.undo = Alpine.addScopeToNode(this, this.reactiveData);
-            this.applySlots();
-            this.root.appendChild(this._component);
-            Alpine.initTree(this.root);
-            this.reactiveData['init'] &&  Alpine.evaluate(this, this.reactiveData['init']);
-            console.log(`[Alpine Component] Finished setup of ${componentName}`);
+            data = { $props: root._m_props, ...data };
+            Alpine.injectMagics(data, root);
+            root._m_reactiveData = Alpine.reactive(data);
+            Alpine.initInterceptors(root._m_reactiveData);
+            root._m_undo = Alpine.addScopeToNode(root, root._m_reactiveData);
+
+            applySlots(this._m_slots, this._m_root);
+            if (this._m_unwrap) this._m_root.append(...this._m_component);
+            else this._m_root.append(this._m_component);
+            Alpine.initTree(this._m_root);
+            root._m_reactiveData['init'] && Alpine.evaluate(root, root._m_reactiveData['init']);
         }
 
         disconnectCallback() {
-            this.reactiveData['destroy'] && Alpine.evaluate(this, this.reactiveData['destroy'])
-            this.undo()
-        }
-
-        attributeChangedCallback() {
-            
-        }
-
-        processComponent(node) {
-            const component = node.tagName === "TEMPLATE" ? node.content.cloneNode(true) : node.cloneNode(true);
-            return { component, slots: Array.from(component.querySelectorAll("slot")) };
-        }
-
-        scopeElements(elements) {
-            Array.from(elements).filter(el => !el._x_dataStack).forEach(el => {
-                console.log("adding scope to ", el);
-                Alpine.addScopeToNode(el, {}, this);
-            });
-        }
-
-        applySlots() {
-            if (this._slots) {
-                const scopable = this.querySelectorAll("*");
-                const defaultContent = Array.from(this.children).filter(el => el.tagName !== 'template');
-                Array.from(this.children).forEach(child => child.remove());
-                const textContent = this.textContent;
-                this.textContent = "";
-                this.scopeElements(scopable);
-                console.log(defaultContent, scopable, textContent);
-                this._slots.forEach(slot => {
-                    if (!slot.hasAttribute("name")) {
-                        slot.replaceWith(...defaultContent, textContent);
-                    }
-                })
-            }
+            console.log("disconnected", componentName, this._m_unwrap);
+            if (this._m_unwrap) return;
+            this._m_reactiveData['destroy'] && Alpine.evaluate(this, this._m_reactiveData['destroy'])
+            this._m_undo()
         }
     }
     try {
--- resources/js/alpine-compositor/utils.js
+++ resources/js/alpine-compositor/utils.js
@@ -1,24 +1,28 @@
-import Alpine from "../alpinejs"
-export const propsBuilder = ($host, props) => {
+import Alpine from "../alpinejs";
+export function setProps(props, defines, $host) {
+    Object.entries(defines).forEach(([prop, config]) => {
+        const defaultData = config.default !== undefined ? config.default : null;
+        if ($host.hasAttribute(prop) || $host.hasAttribute(":" + prop)) {
+            let propData = defaultData;
+            if ($host.hasAttribute(":" + prop)) propData = Alpine.evaluate($host, $host.getAttribute(`:${prop}`)) || null;
+            else if ($host.hasAttribute(prop)) propData = $host.getAttribute(prop) || null;
+            console.log(`Found props[${prop}] = ${propData}`);
+            if (config.type === String && typeof propData !== 'string') console.warn(`prop ${prop} data type should be string`, propData);
+            else if (config.type === Number && typeof propData !== 'number') console.warn(`prop ${prop} data type should be number`, propData);
+            else if (config.type === Array && !Array.isArray(propData)) console.warn(`prop ${prop} data type should be array ${typeof propData}`, propData);
+            else if (config.type === Object && typeof propData !== 'object') console.warn(`prop ${prop} data type should be object`, propData);
+            props[prop] = propData;
+        } else {
+            //console.warn(`props[${prop}] data not found`);
+            props[prop] = defaultData;
+        }
+    });
+}
+
+export function propsBuilder($host, props) {
     return (defines) => {
         console.log(`Defining props for component ${$host.tagName}`);
-        Object.entries(defines).forEach(([prop, config]) => {
-            const defaultData = config.default !== undefined ? config.default : null;
-            if ($host.hasAttribute(prop) || $host.hasAttribute(":" + prop)) {
-                let propData = defaultData;
-                if ($host.hasAttribute(":" + prop)) propData = Alpine.evaluate($host, $host.getAttribute(`:${prop}`)) || null;
-                else if ($host.hasAttribute(prop)) propData = $host.getAttribute(prop) || null;
-                console.log(`Found props[${prop}] = ${propData}`);
-                if (config.type === String && typeof propData !== 'string') console.warn(`prop ${prop} data type should be string`, propData);
-                else if (config.type === Number && typeof propData !== 'number') console.warn(`prop ${prop} data type should be number`, propData);
-                else if (config.type === Array && typeof propData !== 'object') console.warn(`prop ${prop} data type should be array ${typeof propData}`, propData);
-                else if (config.type === Object && typeof propData !== 'object') console.warn(`prop ${prop} data type should be object`, propData);
-                props[prop] = propData;
-            } else {
-                console.warn(`props[${prop}] data not found`);
-                props[prop] = defaultData;
-            }
-        })
+        setProps(props, defines, $host);
         return new Proxy(props, {
             get(target, prop) {
                 return target[prop];
